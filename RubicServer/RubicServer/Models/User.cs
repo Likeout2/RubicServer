bb602ff81f77cf15dcb85142f1d2825dabc01afa
Zhuludkovr <92154194+Likeout2@users.noreply.github.com>
2021-10-08 14:30:35 +0300

    Создал простой сервер на языке json, сделал заметки и вход для пользователя (back-end)
    Создал простой сервер на языке JSON

---
+++ RubicServer/RubicServer/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RubicServer.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string About { get; set; }
+    }
+}
